d11005ef3a90fe1792460764e1de3cbe994f0d96
CrisLopez94 <158295770+CrisLopez94@users.noreply.github.com>
2025-06-20 19:19:43 -0600

    editar usuario

--- src/Screen/Admin/Usuarios/UsuarioCRUD.tsx
+++ src/Screen/Admin/Usuarios/UsuarioCRUD.tsx
@@ -1,8 +1,9 @@
 import { Dialog } from "primereact/dialog";
 import { useForm } from "../../../Hooks/useForm";
-import { Button } from "primereact/button";
 import { supabase } from "../../../supabaseClient";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Dropdown } from "primereact/dropdown";
 
 const UsuarioCRUD = ({
   visible,
@@ -16,6 +17,20 @@ const UsuarioCRUD = ({
   usuarioEditar?: any;
 }) => {
   const editando = !!usuarioEditar;
+
+  const [estados, setEstados] = useState<{ label: string; value: number }[]>(
+    []
+  );
+
+  const fetchEstados = async () => {
+    const { data } = await supabase
+      .from("Estados")
+      .select("IdEstado, NombreEstado");
+    setEstados(
+      data.map((e: any) => ({ label: e.NombreEstado, value: e.IdEstado }))
+    );
+  };
+
   const initialValues = {
     Nombre: "",
     Telefono: "",
@@ -24,7 +39,7 @@ const UsuarioCRUD = ({
     IdEstado: 1,
   };
 
-  const { values, handleInputChange, resetForm, setValues, handleChange } =
+  const { values, handleInputChange, resetForm, setValues } =
     useForm(initialValues);
 
   const guardarUsuario = async () => {
@@ -67,12 +82,23 @@ const UsuarioCRUD = ({
   };
 
   useEffect(() => {
-    if (usuarioEditar) {
-      setValues({ ...usuarioEditar, Password: "" });
-    } else {
-      resetForm();
+    fetchEstados();
+  }, []);
+
+  useEffect(() => {
+    if (visible) {
+      if (usuarioEditar) {
+        setValues({
+          ...usuarioEditar,
+          Password: "",
+          IdEstado: usuarioEditar.IdEstado || 1,
+        });
+      } else {
+        resetForm();
+      }
     }
-  }, [usuarioEditar]);
+  }, [visible]);
+
   return (
     <Dialog
       header={editando ? "Editar Usuario" : "Nuevo Usuario"}
@@ -99,7 +125,69 @@ const UsuarioCRUD = ({
           </button>
         </div>
       }
-    ></Dialog>
+    >
+      <form className="sm:flex sm:flex-wrap flex-col w-full gap-4 mt-4">
+        <div className="flex flex-wrap gap-3 mb-4">
+          <div className="flex-auto">
+            <label htmlFor="nombre" className="font-bold block mb-2">
+              Nombre
+            </label>
+            <InputText
+              id="nombre"
+              name="Nombre"
+              value={values?.Nombre}
+              onChange={handleInputChange}
+              className="w-full"
+            />
+          </div>
+          <div className="flex-auto">
+            <label htmlFor="numero-celular" className="font-bold block mb-2">
+              Numero Celular
+            </label>
+            <InputText
+              id="numero-celular"
+              name="Telefono"
+              value={values?.Telefono}
+              onChange={handleInputChange}
+              keyfilter="num"
+              className="w-full"
+            />
+          </div>
+        </div>
+
+        <div className="sm:flex gap-3 mb-4">
+          <div className="sm:w-1/2 w-full">
+            <label htmlFor="Email" className="font-bold block mb-2">
+              Email
+            </label>
+            <InputText
+              id="Email"
+              name="Email"
+              value={values?.Email}
+              onChange={handleInputChange}
+              className="w-full"
+            />
+          </div>
+
+          <div className="sm:w-1/2 w-full">
+            <label htmlFor="IdEstado" className="font-bold block mb-2">
+              Estado
+            </label>
+            <Dropdown
+              id="IdEstado"
+              name="IdEstado"
+              value={values.IdEstado}
+              options={estados}
+              onChange={handleInputChange}
+              optionLabel="label"
+              optionValue="value"
+              placeholder="Selecciona un estado"
+              className="w-full"
+            />
+          </div>
+        </div>
+      </form>
+    </Dialog>
   );
 };
 
